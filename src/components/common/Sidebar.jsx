d97be45cb2ab954825c204d6a338a53d032fc6de
sleshjan <sleshjan.shakya07@gmail.com>
2025-11-05 22:47:24 +0545

    added go to admin for vercel test

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -54,6 +54,9 @@ const Sidebar = ({ userInterests = [] }) => {
     navigate("/login");
   };
 
+  const gotoAdmin = () =>{
+    navigate("/admin/dashboard");
+  }
   return (
     <div className="w-64 bg-white border-r border-gray-200 h-screen flex flex-col">
       {/* Logo */}
@@ -134,6 +137,15 @@ const Sidebar = ({ userInterests = [] }) => {
             >
               <LogOut className="w-5 h-5" />
               <span className="text-sm font-medium">Logout</span>
+            </button>
+            
+            {/* Admin btn testtttttttttttttttttttttttttttttttttttt*/}
+            <button
+              onClick={gotoAdmin}
+              className="w-full flex items-center gap-3 px-4 py-3 text-red-600 hover:bg-red-50 transition-colors text-left border-t border-gray-100"
+            >
+              <LogOut className="w-5 h-5" />
+              <span className="text-sm font-medium">Go to Admin</span>
             </button>
           </div>
         )}
